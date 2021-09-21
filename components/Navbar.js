85233fe20a032e8e67f4f474e00acc730475e892
webrahul <web.rahul@wingshieldtechnologies.com>
2021-09-21 18:05:06 +0530

    test changes
    installed bootstrap

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,126 @@
-import { AppBar, Button, Grid, Toolbar, Typography } from "@mui/material";
-import { FiLogIn } from "react-icons/fi";
+import Link from "next/link";
+import { useState } from "react";
+import {
+  AppBar,
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Box,
+  // IconButton,
+  Avatar,
+  Badge,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { FiPlusCircle } from "react-icons/fi";
 
+const StyledGrid = styled(Grid)`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1200px;
+  margin: 0 auto;
+  align-items: center;
+`;
+const StyledList = styled(List)`
+  display: flex;
+  justify-content: flex-end;
+  margin-left: 15px !important;
+  li {
+    padding-inline: 10px;
+  }
+`;
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      // animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      boxSizing: "border-box",
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 const Navbar = () => {
+  const [Links, setLinks] = useState([1, 2, 3, 4, 5, 6]);
   return (
     <nav>
       <AppBar position="sticky">
         <Toolbar>
-          <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <Typography variant="h5" component="h1" sx={{ flexGrow: 1 }}>
-                LOGO
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={8}
-              container
-              direction="row"
-              justifyContent="flex-end"
-              alignItems="center"
+          <StyledGrid container spacing={2}>
+            <Box
+              component="div"
+              style={{ display: "flex", alignItems: "center" }}
+              sx={{ flexGrow: 1 }}
             >
-              <Button color="secondary" variant="contained">
-                Login &nbsp;
-                <FiLogIn />
-              </Button>
-            </Grid>
-          </Grid>
+              <Link href="/" underline="none" passHref>
+                <Typography variant="h5" component="a">
+                  LOGO
+                </Typography>
+              </Link>
+              <StyledList style={{ marginLeft: "20px" }}>
+                {Links.map((li, idx) => (
+                  <ListItem disablePadding key={idx}>
+                    <Link href="/" underline="none" passHref>
+                      <Typography variant="span" component="a">
+                        About
+                      </Typography>
+                    </Link>
+                  </ListItem>
+                ))}
+              </StyledList>
+            </Box>
+            <StyledList>
+              <ListItem disablePadding>
+                <ListItemButton disableRipple>
+                  <StyledBadge
+                    overlap="circular"
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                    variant="dot"
+                    // sx={{
+                    //   boxShadow: 3,
+                    // }}
+                  >
+                    <Avatar>H</Avatar>
+                  </StyledBadge>
+                  <ListItemText
+                    primary="My Account"
+                    style={{ marginInline: "10px", whiteSpace: "nowrap" }}
+                  />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  style={{ borderRadius: "30px" }}
+                >
+                  Add Listing <FiPlusCircle style={{ marginLeft: "5px" }} />
+                </Button>
+              </ListItem>
+            </StyledList>
+          </StyledGrid>
         </Toolbar>
       </AppBar>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Head from "next/head";
+import "../public/assets/css/bootstrap.min.css";
+// import "../public/assets/js/bootstrap.bundle.min.js";
 import "../styles/globals.css";
 
 const theme = createTheme({
   palette: {
+    mode: "light",
     primary: {
-      main: "#bf360c",
-      light: "#cb5e3c",
-      dark: "#852508",
+      main: "#ee5a2d",
+      light: "#ffa58a",
+      dark: "#ff3c00",
     },
     secondary: {
       main: "#29b6f6",
@@ -19,6 +23,9 @@ const theme = createTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
